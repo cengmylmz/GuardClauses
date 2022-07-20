003e10bf93fef34248718e56b98ff902a8da0006
cengmylmz <cengmylmz@gmail.com>
2022-07-21 00:16:23 +0300

    guard was added

--- GuardClauses/Guard.cs
+++ GuardClauses/Guard.cs
@@ -1,11 +1,34 @@
 ﻿using System;
 namespace GuardClauses
 {
-	public class Guard
-	{
-		public Guard()
-		{
-		}
-	}
+    public static class Guard
+    {
+
+        public static IGuardClause<T> SetValue<T>(T instance)
+            where T : class, new()
+        {
+            return new GuardClause<T>(instance);
+        }
+
+        public static IGuardClause<T, T1> SetValue<T, T1>(T instance, T1 instance1)
+            where T : class, new()
+            where T1 : class, new()
+        {
+            return new GuardClause<T, T1>(instance, instance1);
+        }
+
+        public static IGuardClause<T> SetValue<T>(T instance, string exceptionMessagePrefix)
+            where T : class, new()
+        {
+            return new GuardClause<T>(instance, exceptionMessagePrefix);
+        }
+
+        public static IGuardClause<T, T1> SetValue<T, T1>(T instance, T1 instance1, string exceptionMessagePrefix)
+            where T : class, new()
+            where T1 : class, new()
+        {
+            return new GuardClause<T, T1>(instance, instance1, exceptionMessagePrefix);
+        }
+    }
 }
 
